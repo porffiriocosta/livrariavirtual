8fea48dd5eefe96265bb23f538798cfba5f085bf
Reginaldo P Costa <porffiriocosta@gmail.com>
2022-07-15 15:00:15 -0300

    Mudança nas configurações de publish

---
+++ front-end/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrlv1: 'https://livrariavirtualapi.azurewebsites.net/api/v1/',
+  imagensUrl: 'https://livrariavirtualapi.azurewebsites.net/'
+};
