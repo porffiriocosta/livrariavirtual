16ab965561009e8bb180083b730373f2208c5adb
Reginaldo P Costa <porffiriocosta@gmail.com>
2022-07-21 09:15:53 -0300

    Alteração no enviroment

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlv1: 'https://www.livrariavirtual.net.br/api/v1/',
-  imagensUrl: 'https://www.livrariavirtual.net.br/'
+  apiUrlv1: 'https://livrariavirtual.net.br/api/v1/',
+  imagensUrl: 'https://livrariavirtual.net.br/'
 };
